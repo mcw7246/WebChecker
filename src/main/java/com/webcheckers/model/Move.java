73a8e6b57060c3815c3b52b6a0f17c8d3840d340
Austin Miller <akm8654@rit.edu>
2020-03-29 14:16:14 -0400

    Implemented base idea for validating a move.

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -2,6 +2,9 @@ package com.webcheckers.model;
 
 import com.webcheckers.ui.PostValidateMoveRoute.MoveStatus;
 
+import java.lang.Math.*;
+import java.util.Objects;
+
 /**
  * Uses a getStart and getEnd for a position model.
  *
@@ -38,15 +41,72 @@ public class Move
     return end;
   }
 
+  /**
+   * Validates that a move is correct.
+   *
+   * @param game the game that is being played
+   * @return a status based on what was found.
+   */
   public MoveStatus validateMove(CheckerGame game)
   {
     BoardView board = game.getBoardView();
     Space startSpace = board.getSpaceAt(start.getRow(), start.getCell());
     Space endSpace = board.getSpaceAt(end.getRow(), end.getCell());
 
-    if (!endSpace.isValidSpace())
+    Piece piece = startSpace.getPiece();
+    boolean king = piece.getType().equals(Piece.Type.KING);
+
+    Objects.requireNonNull(piece, "piece cannot be null at the start of a " +
+            "move");
+
+    int rowDiff = (end.getRow() - start.getRow());
+    int colDiff = (end.getCell() - start.getCell());
+
+    if (!endSpace.isValidSpace()) //is the space the correct color?
     {
       return MoveStatus.INVALID_SPACE;
+    } else if (startSpace.equals(endSpace)) //is the space the same as the
+    // start?
+    {
+      return MoveStatus.SAME_SPACE;
+    } else
+    {
+      if (endSpace.getPiece() != null) // there's already a piece there!
+      {
+        return MoveStatus.OCCUPIED;
+      }
+      if (Math.abs(rowDiff) > 1 || Math.abs(colDiff) > 1) //more than 1 by 1
+      // away.
+      {
+        if (Math.abs(rowDiff) > 2 || Math.abs(colDiff) > 2) // more than 2
+        // away in any direction.
+        {
+          return MoveStatus.TOO_FAR;
+        }
+        Piece jumpPiece = board.getSpaceAt(start.getRow() + (rowDiff / 2),
+                start.getCell() + (colDiff / 2)).getPiece();
+        if (jumpPiece.getColor().equals(piece.getColor()))
+        {
+          return MoveStatus.JUMP_OWN;
+        } else if (Math.abs(rowDiff) != Math.abs(colDiff))
+        {
+          return MoveStatus.INVALID_DIR;
+        } else if (king)
+        {
+          return MoveStatus.VALID;
+        } else if (rowDiff < 0)
+        {
+          return MoveStatus.INVALID_BACKWARDS;
+        }
+      }
+      if (king)
+      {
+        return MoveStatus.VALID;
+      } else if (rowDiff < 0)
+      {
+        return MoveStatus.INVALID_BACKWARDS;
+      }
     }
+    return MoveStatus.VALID;
   }
 }
--- src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -1,10 +1,16 @@
 package com.webcheckers.ui;
 
 import com.google.gson.Gson;
+import com.webcheckers.application.PlayerLobby;
+import com.webcheckers.model.CheckerGame;
 import com.webcheckers.model.Move;
+import com.webcheckers.model.Player;
 import spark.*;
 
 import java.util.Objects;
+
+import static com.webcheckers.ui.PostRequestGameRoute.MESSAGE;
+import static spark.Spark.halt;
 
 /**
  * This action submits a single move for validation. The server must keep
@@ -18,7 +24,9 @@ import java.util.Objects;
  */
 public class PostValidateMoveRoute implements Route
 {
-  public enum MoveStatus {INVALID_SPACE, VALID, OCCUPIED, TOO_FAR}
+  /** All the types of moves */
+  public enum MoveStatus {INVALID_SPACE, VALID, OCCUPIED, TOO_FAR, SAME_SPACE
+    , INVALID_BACKWARDS, JUMP_OWN, INVALID_DIR}
 
   Gson gson = new Gson();
   private static final String ACTION_DATA = "actionData";
@@ -32,12 +40,85 @@ public class PostValidateMoveRoute implements Route
     this.templateEngine = templateEngine;
   }
 
+  /**
+   * Helper method for setting the error message on teh game screen
+   *
+   * @param session: the HTTP session
+   * @param message: the message to be sent.
+   */
+  private void setErrorMsg(Session session, String message)
+  {
+    session.attribute(GetHomeRoute.ERROR_MESSAGE_KEY, message);
+  }
+
+  /**
+   * Helper method for setting the message on the game screen when a move
+   * is valid.
+   *
+   * @param session the HTTP session
+   * @param message the message to be sent.
+   */
+  private void setMsg(Session session, String message)
+  {
+    session.attribute(MESSAGE, message);
+  }
+
   @Override
   public String handle(Request request, Response response)
   {
     final Session httpSession = request.session();
     final String moveStr = request.queryParams(ACTION_DATA);
     final Move move = gson.fromJson(moveStr, Move.class);
+    final PlayerLobby lobby = httpSession.attribute(GetHomeRoute.PLAYER_LOBBY_KEY);
+    final Player player = httpSession.attribute(GetHomeRoute.PLAYER_KEY);
+    final CheckerGame game = lobby.getGame(player.getUsername());
+
+    if (lobby != null && game != null)
+    {
+      MoveStatus moveValidity = move.validateMove(game);
+      String msg;
+      switch (moveValidity)
+      {
+        case INVALID_SPACE:
+          msg = "Invalid Move: Space is the wrong color!";
+          setErrorMsg(httpSession, msg);
+          break;
+        case VALID:
+          break;
+        case OCCUPIED:
+          msg = "Invalid Move: Space is already Occupied!";
+          setErrorMsg(httpSession, msg);
+          break;
+        case TOO_FAR:
+          msg = "Invalid Move: Space is too far away!";
+          setErrorMsg(httpSession, msg);
+          break;
+        case SAME_SPACE:
+          msg = "Invalid Move: The Space you've tried to move to is the same " +
+                  "one!";
+          setErrorMsg(httpSession, msg);
+          break;
+        case INVALID_BACKWARDS:
+          msg = "Invalid Move: You aren't kinged yet! You must move forward!";
+          setErrorMsg(httpSession, msg);
+          break;
+        case JUMP_OWN:
+          msg = "Invalid Move: You're attempting to jump your own piece!";
+          setErrorMsg(httpSession, msg);
+          break;
+        case INVALID_DIR:
+          msg = "Invalid Move: You must move diagonally!";
+          setErrorMsg(httpSession, msg);
+          break;
+      }
+
+      response.redirect(WebServer.GAME_URL);
+    } else
+    {
+      response.redirect(WebServer.HOME_URL);
+      halt();
+    }
+    return null;
 
   }
 }
