6ab4d496ef0454b678bad30d164d024de7f9693d
Sean Bergen <sdb2139@g.rit.edu>
2020-04-14 22:06:25 -0400

    Having some problems with redirecting (again), but I think the code for this is almost to the point of being done

--- src/main/java/com/webcheckers/ui/PostResignRoute.java
+++ src/main/java/com/webcheckers/ui/PostResignRoute.java
@@ -1,26 +1,53 @@
 package com.webcheckers.ui;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
+import com.google.gson.Gson;
+import com.webcheckers.model.Player;
+import javafx.geometry.Pos;
+import spark.*;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.webcheckers.ui.GetHomeRoute.PLAYER_KEY;
+import static com.webcheckers.ui.GetHomeRoute.VIEW_NAME;
+import static com.webcheckers.ui.WebServer.HOME_URL;
 
 public class PostResignRoute implements Route
 {
     final Map<String, Object> modeOptions = new HashMap<>(2);
 
+    Gson gson = new Gson();
+
+    final TemplateEngine templateEngine;
+
+
+    public PostResignRoute(TemplateEngine templateEngine)
+    {
+        this.templateEngine = templateEngine;
+    }
+
+
     @Override
     public Object handle(Request request, Response response) throws Exception
     {
+        final Session session = request.session();
+        final Player player = session.attribute(PLAYER_KEY);
+
+        final Map<String, Object> vm = new HashMap<>();
+
+        modeOptions.put("isGameOver", true);
+        modeOptions.put("gameOverMessage",player.getUsername() + " has resigned.");
+
+        vm.put("modeOptionsAsJSON", gson.toJson(modeOptions));
+
+
+
+        response.redirect(WebServer.HOME_URL);
+
+
+
+        //return templateEngine.render(new ModelAndView(vm, VIEW_NAME));
         return null;
     }
-
-/**
-    modeOptions.put("isGameOver", true);
-    modeOptions.put("gameOverMessage", );
-    m.put("modeOptionsAsJSON", gson.toJson(modeOptions));
-**/
 
 }
--- src/main/java/com/webcheckers/ui/WebServer.java
+++ src/main/java/com/webcheckers/ui/WebServer.java
@@ -66,6 +66,7 @@ public class WebServer
   public static final String CHECK_TURN_URL = "/checkTurn";
   public static final String SUBMIT_TURN_URL = "/submitTurn";
   public static final String BACKUP_MOVE_URL = "/backupMove";
+  public static final String RESIGN_GAME_URL = "/resignGame";
 
   //
   // Attributes
@@ -156,6 +157,7 @@ public class WebServer
     post(VALIDATE_MOVE_URL, new PostValidateMoveRoute());
     post(SUBMIT_TURN_URL, new PostSubmitTurnRoute());
     post(BACKUP_MOVE_URL, new PostBackupMoveRoute());
+    post(RESIGN_GAME_URL, new PostResignRoute(templateEngine));
     LOG.config("WebServer is initialized.");
   }
 
