4ee30d436143f66ac01c960d7ef3e46fde08f2ff
Austin Miller <akm8654@rit.edu>
2020-04-17 04:16:21 -0700

    Updated coverage for GetGameRoute

--- src/test/java/com/webcheckers/ui/GetGameRouteTest.java
+++ src/test/java/com/webcheckers/ui/GetGameRouteTest.java
@@ -6,6 +6,7 @@ import com.webcheckers.application.PlayerLobby;
 import com.webcheckers.application.ReplayManager;
 import com.webcheckers.model.Board;
 import com.webcheckers.model.CheckerGame;
+import com.webcheckers.model.Piece;
 import com.webcheckers.model.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
@@ -18,7 +19,12 @@ import java.io.FileReader;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.webcheckers.ui.GetGameRoute.ACTIVE_COLOR;
+import static com.webcheckers.ui.GetHomeRoute.GAME_MANAGER_KEY;
+import static com.webcheckers.ui.GetHomeRoute.PLAYER_KEY;
 import static com.webcheckers.ui.GetReplayGameRoute.MODE_OPTIONS_AS_JSON;
+import static com.webcheckers.ui.PostRequestResponseRoute.NO_MORE_MOVES;
+import static com.webcheckers.ui.PostSubmitTurnRouteTest.INITIAL_BOARD;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -48,10 +54,11 @@ public class GetGameRouteTest
   private GameManager manager;
   private Player player1, player2;
   private ReplayManager rManager;
-  private CheckerGame game;
   private Board board;
+  TemplateEngineTest testHelper;
 
   //Mock objects
+  private CheckerGame game;
   private Request request;
   private Session session;
   private Response response;
@@ -63,7 +70,7 @@ public class GetGameRouteTest
   @BeforeEach
   public void setup()
   {
-
+    testHelper = new TemplateEngineTest();
     lobby = new PlayerLobby();
     rManager = new ReplayManager();
     manager = new GameManager(lobby, rManager);
@@ -77,6 +84,7 @@ public class GetGameRouteTest
     when(request.session()).thenReturn(session);
     response = mock(Response.class);
     engine = mock(TemplateEngine.class);
+    board = mock(Board.class);
 
     CuT = new GetGameRoute(engine);
   }
@@ -92,7 +100,7 @@ public class GetGameRouteTest
     manager.startGame(player1.getUsername(), player2.getUsername());
 
     when(request.session()).thenReturn(session);
-    when(session.attribute(GetHomeRoute.PLAYER_KEY)).thenReturn(player1);
+    when(session.attribute(PLAYER_KEY)).thenReturn(player1);
     when(session.attribute(GetHomeRoute.GAME_MANAGER_KEY)).thenReturn(manager);
 
     TemplateEngineTest testHelper = new TemplateEngineTest();
@@ -113,7 +121,7 @@ public class GetGameRouteTest
     manager.startGame(player1.getUsername(), player2.getUsername());
 
     when(request.session()).thenReturn(session);
-    when(session.attribute(GetHomeRoute.PLAYER_KEY)).thenReturn(player2);
+    when(session.attribute(PLAYER_KEY)).thenReturn(player2);
     when(session.attribute(GetHomeRoute.GAME_MANAGER_KEY)).thenReturn(manager);
 
     TemplateEngineTest testHelper = new TemplateEngineTest();
@@ -134,7 +142,7 @@ public class GetGameRouteTest
     manager.startGame(player1.getUsername(), player2.getUsername());
 
     when(request.session()).thenReturn(session);
-    when(session.attribute(GetHomeRoute.PLAYER_KEY)).thenReturn(null);
+    when(session.attribute(PLAYER_KEY)).thenReturn(null);
     when(session.attribute(GetHomeRoute.GAME_MANAGER_KEY)).thenReturn(manager);
 
     TemplateEngineTest testHelper = new TemplateEngineTest();
@@ -151,7 +159,7 @@ public class GetGameRouteTest
   public void null_game_test()
   {
     when(request.session()).thenReturn(session);
-    when(session.attribute(GetHomeRoute.PLAYER_KEY)).thenReturn(player1);
+    when(session.attribute(PLAYER_KEY)).thenReturn(player1);
     GameManager MANAGER = mock(GameManager.class);
     when(session.attribute(GetHomeRoute.GAME_MANAGER_KEY)).thenReturn(MANAGER);
     when(MANAGER.getGame(any(Integer.class))).thenReturn(null);
@@ -159,20 +167,16 @@ public class GetGameRouteTest
     assertNull(CuT.handle(request, response));
   }
 
+  /**
+   * Test if red won doing something random
+   */
   @Test
-  public void cannot_move()
-  {
-    assertNotNull(CuT.handle(request, response));
-  }
-
-  @Test
-  public void test_you_won()
+  public void test_red_won()
   {
     try
     {
       board = gson.fromJson(new FileReader(RED_WINS_BOARD), Board.class);
-    }
-    catch (FileNotFoundException e)
+    } catch (FileNotFoundException e)
     {
       fail("ERROR: FILE NOT FOUND STARTING GAME FROM SCRATCH");
       board = new Board();
@@ -181,31 +185,273 @@ public class GetGameRouteTest
     GameManager manager = mock(GameManager.class);
     game = new CheckerGame(player1, player2, board);
 
-    int gameID = manager.getGameID(player1.getUsername());
-
     int redPieces = game.getNumRedPieces();
-    int whitePieces = game.getNumWhitePieces();
-    when(session.attribute(GetHomeRoute.PLAYER_KEY)).thenReturn(player1);
+    when(session.attribute(PLAYER_KEY)).thenReturn(player1);
     when(session.attribute(GetHomeRoute.GAME_MANAGER_KEY)).thenReturn(manager);
     when(manager.getGame(gameID)).thenReturn(game);
     when(manager.getOpponent(player1.getUsername())).thenReturn(player2);
-    System.out.println(manager.getGameID(player1.getUsername()));
-    System.out.println(whitePieces);
-    System.out.println(redPieces);
     assertEquals(0, redPieces);
-    Map<String, Object> modeOptions = new HashMap<>();
 
 
-    //CuT.handle(request, response);
+    when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+    CuT.handle(request, response);
 
-    System.out.println();
-    TemplateEngineTest testHelper = new TemplateEngineTest();
-    //testHelper.assertViewModelAttribute(MODE_OPTIONS_AS_JSON, gson.toJson(modeOptions));
-    //testHelper.assertViewModelAttribute(GetGameRoute.RED_PLAYER, redPlayer);
-    //when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+    //now go as other player
 
-
-    //when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
   }
 
+  /**
+   * Jump the last white piece as a red piece.
+   */
+  @Test
+  public void jump_last_red()
+  {
+    GameManager manager = mock(GameManager.class);
+    game = mock(CheckerGame.class);
+    when(session.attribute(GAME_MANAGER_KEY)).thenReturn(manager);
+    when(manager.getGameID(player1.getUsername())).thenReturn(gameID);
+    when(session.attribute(PLAYER_KEY)).thenReturn(player1);
+    when(manager.getGame(gameID)).thenReturn(game);
+    when(manager.getOpponent(player1.getUsername())).thenReturn(player2);
+
+    try
+    {
+      board = gson.fromJson(new FileReader(INITIAL_BOARD), Board.class);
+    } catch (FileNotFoundException e)
+    {
+      fail("ERROR: FILE NOT FOUND STARTING GAME FROM SCRATCH");
+      board = new Board();
+    }
+
+    when(game.getColor()).thenReturn(Piece.Color.RED);
+    when(game.getWhitePlayer()).thenReturn(player2);
+    when(game.getRedPlayer()).thenReturn(player1);
+    when(game.getBoard()).thenReturn(board);
+
+    when(manager.getGameID(player1.getUsername())).thenReturn(gameID);
+    when(session.attribute("theme")).thenReturn("pink");
+
+    when(game.getNumRedPieces()).thenReturn(4);
+    when(game.getNumWhitePieces()).thenReturn(0);
+
+    player1.hasEnteredGame();
+
+    when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+    CuT.handle(request, response);
+
+    testHelper.assertViewModelExists();
+    testHelper.assertViewModelAttribute(ACTIVE_COLOR, Piece.Color.RED);
+    final Map<String, Object> modeOptions = new HashMap<>(2);
+    String gameOverMessage = player2.getUsername() + "'s pieces " +
+            "were all taken! You win!";
+    modeOptions.put("isGameOver", true);
+    modeOptions.put("gameOverMessage", gameOverMessage);
+
+    testHelper.assertViewModelAttribute("modeOptionsAsJSON", gson.toJson(modeOptions));
+  }
+
+  /**
+   * Jump the last white piece
+   */
+  @Test
+  public void jump_last_white()
+  {
+    GameManager manager = mock(GameManager.class);
+    game = mock(CheckerGame.class);
+    when(session.attribute(GAME_MANAGER_KEY)).thenReturn(manager);
+    when(manager.getGameID(player1.getUsername())).thenReturn(gameID);
+    when(session.attribute(PLAYER_KEY)).thenReturn(player1);
+    when(manager.getGame(gameID)).thenReturn(game);
+    when(manager.getOpponent(player1.getUsername())).thenReturn(player2);
+
+    try
+    {
+      board = gson.fromJson(new FileReader(INITIAL_BOARD), Board.class);
+    } catch (FileNotFoundException e)
+    {
+      fail("ERROR: FILE NOT FOUND STARTING GAME FROM SCRATCH");
+      board = new Board();
+    }
+
+    when(game.getColor()).thenReturn(Piece.Color.WHITE);
+    when(game.getWhitePlayer()).thenReturn(player2);
+    when(game.getRedPlayer()).thenReturn(player1);
+    when(game.getBoard()).thenReturn(board);
+
+    when(manager.getGameID(player1.getUsername())).thenReturn(gameID);
+
+    when(game.getNumRedPieces()).thenReturn(0);
+    when(game.getNumWhitePieces()).thenReturn(4);
+
+    player1.hasEnteredGame();
+
+    when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+    CuT.handle(request, response);
+
+    testHelper.assertViewModelExists();
+    testHelper.assertViewModelAttribute(ACTIVE_COLOR, Piece.Color.WHITE);
+    final Map<String, Object> modeOptions = new HashMap<>(2);
+    String gameOverMessage = player1.getUsername() + ", Your pieces were all " +
+            "taken! You lose!";
+    modeOptions.put("isGameOver", true);
+    modeOptions.put("gameOverMessage", gameOverMessage);
+    testHelper.assertViewModelAttribute("modeOptionsAsJSON", gson.toJson(modeOptions));
+  }
+
+  /**
+   * Test for no moves left for the opponent
+   */
+  @Test
+  public void test_no_more_moves()
+  {
+    GameManager manager = mock(GameManager.class);
+    game = mock(CheckerGame.class);
+    when(session.attribute(GAME_MANAGER_KEY)).thenReturn(manager);
+    when(manager.getGameID(player1.getUsername())).thenReturn(gameID);
+    when(session.attribute(PLAYER_KEY)).thenReturn(player1);
+    when(manager.getGame(gameID)).thenReturn(game);
+    when(manager.getOpponent(player1.getUsername())).thenReturn(player2);
+
+    try
+    {
+      board = gson.fromJson(new FileReader(NO_MORE_MOVES), Board.class);
+    } catch (FileNotFoundException e)
+    {
+      fail("ERROR: FILE NOT FOUND STARTING GAME FROM SCRATCH");
+      board = new Board();
+    }
+
+    when(game.getColor()).thenReturn(Piece.Color.WHITE);
+    when(game.getWhitePlayer()).thenReturn(player2);
+    when(game.getRedPlayer()).thenReturn(player1);
+    when(game.getBoard()).thenReturn(board);
+
+    when(manager.getGameID(player1.getUsername())).thenReturn(gameID);
+
+    when(game.getNumRedPieces()).thenReturn(3);
+    when(game.getNumWhitePieces()).thenReturn(4);
+
+    player1.hasEnteredGame();
+
+    when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+    CuT.handle(request, response);
+
+    testHelper.assertViewModelExists();
+    testHelper.assertViewModelAttribute(ACTIVE_COLOR, Piece.Color.WHITE);
+    final Map<String, Object> modeOptions = new HashMap<>(2);
+    String gameOverMessage = player2.getUsername() + " cannot move! You win!";
+    modeOptions.put("isGameOver", true);
+    modeOptions.put("gameOverMessage", gameOverMessage);
+    testHelper.assertViewModelAttribute("modeOptionsAsJSON", gson.toJson(modeOptions));
+  }
+
+  /**
+   * Creates tests for no moves being left for the current user.
+   */
+  @Test
+  public void test_no_more_moves_white()
+  {
+    GameManager manager = mock(GameManager.class);
+    game = mock(CheckerGame.class);
+    when(session.attribute(GAME_MANAGER_KEY)).thenReturn(manager);
+    when(manager.getGameID(player2.getUsername())).thenReturn(gameID);
+    when(session.attribute(PLAYER_KEY)).thenReturn(player2);
+    when(manager.getGame(gameID)).thenReturn(game);
+    when(manager.getOpponent(player2.getUsername())).thenReturn(player1);
+
+    try
+    {
+      board = gson.fromJson(new FileReader(NO_MORE_MOVES), Board.class);
+    } catch (FileNotFoundException e)
+    {
+      fail("ERROR: FILE NOT FOUND STARTING GAME FROM SCRATCH");
+      board = new Board();
+    }
+
+    when(game.getColor()).thenReturn(Piece.Color.RED);
+    when(game.getWhitePlayer()).thenReturn(player2);
+    when(game.getRedPlayer()).thenReturn(player1);
+    when(game.getBoard()).thenReturn(board);
+
+    when(manager.getGameID(player1.getUsername())).thenReturn(gameID);
+
+    when(game.getNumRedPieces()).thenReturn(3);
+    when(game.getNumWhitePieces()).thenReturn(4);
+
+    player1.hasEnteredGame();
+
+    when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+    CuT.handle(request, response);
+
+    testHelper.assertViewModelExists();
+    testHelper.assertViewModelAttribute(ACTIVE_COLOR, Piece.Color.RED);
+    final Map<String, Object> modeOptions = new HashMap<>(2);
+    String gameOverMessage = player1.getUsername() + " has stopped you from " +
+            "moving! You've lost!";
+    modeOptions.put("isGameOver", true);
+    modeOptions.put("gameOverMessage", gameOverMessage);
+    testHelper.assertViewModelAttribute("modeOptionsAsJSON", gson.toJson(modeOptions));
+  }
+
+  /**
+   * Produces a test for both users resigning (one at a time though)
+   */
+  @Test
+  public void test_resign()
+  {
+    GameManager manager = mock(GameManager.class);
+    game = mock(CheckerGame.class);
+    when(session.attribute(GAME_MANAGER_KEY)).thenReturn(manager);
+    when(manager.getGameID(player1.getUsername())).thenReturn(gameID);
+    when(session.attribute(PLAYER_KEY)).thenReturn(player1);
+    when(manager.getGame(gameID)).thenReturn(game);
+    when(manager.getOpponent(player1.getUsername())).thenReturn(player2);
+
+    try
+    {
+      board = gson.fromJson(new FileReader(INITIAL_BOARD), Board.class);
+    } catch (FileNotFoundException e)
+    {
+      fail("ERROR: FILE NOT FOUND STARTING GAME FROM SCRATCH");
+      board = new Board();
+    }
+
+    when(game.getColor()).thenReturn(Piece.Color.WHITE);
+    when(game.getWhitePlayer()).thenReturn(player2);
+    when(game.getRedPlayer()).thenReturn(player1);
+    when(game.getBoard()).thenReturn(board);
+
+    when(manager.getGameID(player1.getUsername())).thenReturn(gameID);
+
+    when(game.getNumRedPieces()).thenReturn(4);
+    when(game.getNumWhitePieces()).thenReturn(4);
+
+    when(manager.getGameOverStatus(gameID)).thenReturn(player1.getUsername() +
+            " has resigned.");
+    player1.hasEnteredGame();
+
+    when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+    CuT.handle(request, response);
+
+    testHelper.assertViewModelExists();
+    testHelper.assertViewModelAttribute(ACTIVE_COLOR, Piece.Color.WHITE);
+    Map<String, Object> modeOptions = new HashMap<>(2);
+    String gameOverMessage = player1.getUsername() + " has resigned.";
+    modeOptions.put("isGameOver", true);
+    modeOptions.put("gameOverMessage", gameOverMessage);
+    testHelper.assertViewModelAttribute("modeOptionsAsJSON", gson.toJson(modeOptions));
+
+    when(manager.getGameOverStatus(gameID)).thenReturn(player2.getUsername() +
+            " has resigned.");
+
+    when(engine.render(any(ModelAndView.class))).thenAnswer(testHelper.makeAnswer());
+    CuT.handle(request, response);
+
+    testHelper.assertViewModelExists();
+    modeOptions = new HashMap<>(2);
+    gameOverMessage = player2.getUsername() + " has resigned.";
+    modeOptions.put("isGameOver", true);
+    modeOptions.put("gameOverMessage", gameOverMessage);
+    testHelper.assertViewModelAttribute("modeOptionsAsJSON", gson.toJson(modeOptions));
+  }
 }
